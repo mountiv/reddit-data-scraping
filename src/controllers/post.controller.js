37ae1e47a8d60a0df7051ec976157fc22236e47b
mountiv <silviomavrel@gmail.com>
2023-03-05 13:35:41 +0100

    developed reddit post api

--- src/controllers/post.controller.js
+++ src/controllers/post.controller.js
@@ -37,4 +37,61 @@ module.exports = {
 
     res.send("recent posts saving triggered.");
   },
+  getPosts: async function (req, res) {
+    const limit = req.query.limit == undefined ? 25 : req.query.limit;
+    const offset = req.query.offset == undefined ? 0 : req.query.offset;
+    const filter =
+      req.query.filter == undefined ? {} : JSON.parse(req.query.filter);
+
+    const posts = await Post.find(filter).limit(limit).skip(offset).exec();
+    if (posts.length > 0) {
+      res.json({ result: true, data: posts });
+    } else {
+      res.json({ result: false, data: "not found posts." });
+    }
+  },
+  getLastPosts: async function (req, res) {
+    const limit = req.query.limit == undefined ? 25 : req.query.limit;
+    const offset = req.query.offset == undefined ? 25 : req.query.offset;
+    const filter =
+      req.query.filter == undefined ? {} : JSON.parse(req.query.filter);
+    const new_filter = Object.assign(filter, {
+      "data.subreddit": req.params.reddit,
+    });
+    const posts = await Post.find(new_filter).limit(limit).skip(offset).exec();
+    if (posts.length > 0) {
+      res.json({ result: true, data: posts });
+    } else {
+      res.json({ result: false, data: "not found posts." });
+    }
+  },
+  getPost: async function (req, res) {
+    const post = await Post.findOne({ "data.id": req.params.id });
+    if (!post) {
+      res.json({ result: false, data: "no post." });
+    } else {
+      res.json({ result: true, data: post });
+    }
+  },
+  deletePost: async function (req, res) {
+    const result = await Post.deleteOne({ "data.id": req.params.id });
+    if (result.deletedCount == 0) {
+      res.json({ result: false, data: "no such post" });
+    } else {
+      res.json({ result: true, data: "post deleted" });
+    }
+  },
+  searchPosts: async function (req, res) {
+    const limit = req.query.limit == undefined ? 25 : req.query.limit;
+    const offset = req.query.offset == undefined ? 25 : req.query.offset;
+    const filter =
+      req.query.filter == undefined ? {} : JSON.parse(req.query.filter);
+
+    const posts = await Post.find(filter).limit(limit).skip(offset).exec();
+    if (posts.length > 0) {
+      res.json({ result: true, data: posts });
+    } else {
+      res.json({ result: false, data: "not found posts." });
+    }
+  },
 };
---
+++ src/routes/post.router.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const router = express.Router();
+const controller = require("../controllers/post.controller");
+
+router.get("/posts", function (req, res) {
+  controller.getPosts(req, res);
+});
+
+router.get("/posts/:reddit", function (req, res) {
+  controller.getLastPosts(req, res);
+});
+
+router.get("/post/:id", function (req, res) {
+  controller.getPost(req, res);
+});
+
+router.delete("/post/:id", function (req, res) {
+  controller.deletePost(req, res);
+});
+
+router.get("/search", function (req, res) {
+  controller.searchPosts(req, res);
+});
+
+module.exports = router;
