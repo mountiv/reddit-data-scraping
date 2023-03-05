1a6ed5f6a6d7356d2e9ee6966f5148fd250d0378
mountiv <silviomavrel@gmail.com>
2023-03-05 12:28:04 +0100

    developed reddit api

--- src/controllers/reddit.controller.js
+++ src/controllers/reddit.controller.js
@@ -29,5 +29,18 @@ module.exports = {
   saveReddits: async function (req, res) {
     saveAllReddits("ThisIsMyRandomString...");
     res.send("all reddits saving triggered.");
-  }
+  },
+  getReddits: async function (req, res) {
+    const limit = req.query.limit == undefined ? 25 : req.query.limit;
+    const offset = req.query.offset == undefined ? 0 : req.query.offset;
+    const filter =
+      req.query.filter == undefined ? {} : JSON.parse(req.query.filter);
+    const reddits = await Reddit.find(filter).limit(limit).skip(offset).exec();
+
+    if (reddits.length > 0) {
+      res.status(200).json({ result: true, data: reddits });
+    } else {
+      res.status(400).json({ result: false, data: "not found reddits." });
+    }
+  },
 };
